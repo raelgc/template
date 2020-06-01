4aa0d8beb1ecf1859456ccd6de8310b604666766
Rael Gugelmin Cunha <rael.gc@gmail.com>
2020-06-01 13:52:41 +0000

    Draft PHPUnit support (#42)

---
+++ tests/README.md
@@ -0,0 +1,3 @@
+Run:
+
+    ./vendor/bin/phpunit tests
---
+++ tests/TemplateTest.php
@@ -0,0 +1,15 @@
+<?php declare(strict_types=1);
+require_once('raelgc/view/Template.php');
+use PHPUnit\Framework\TestCase;
+use raelgc\view\Template;
+
+final class TemplateTest extends TestCase {
+
+	public function testSimpleVar(): void {
+		$tpl = new Template(__DIR__.'/simple_var.html' );
+		$tpl->FOO = 'bar';
+		$this->assertEquals(trim($tpl->parse()), 'bar');
+	}
+
+}
+
